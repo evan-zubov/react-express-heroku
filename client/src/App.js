5e519c50334857bfd53599e9958848e2a7b36de2
Matteo Manzinello <matteo@manzinello.com>
2018-06-05 09:30:48 +0200

    Pulizia del codice

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {cities: []}
+  state = { cities: [] }
 
   async componentDidMount() {
     const response = await fetch('/cities')
-    const cities   = await response.json()
+    const cities = await response.json()
 
-    this.setState({cities: cities})
+    this.setState({ cities: cities })
   }
 
   render() {
     return (
       <div>
         <ul>
-          {this.state.cities.map( city => {
+          {this.state.cities.map(city => {
             return <li key={city.name}> <b>{city.name}</b>: {city.population}</li>
           })}
         </ul>
